baa17609ab0d56153059f082601d860b4fa0cb4c
BrivaHamisi <brivahamisi@gmail.com>
2025-05-02 15:25:20 +0300

    Database Models

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Jane Smith',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2023'
-    },
-    {
-        'author': 'Alice Johnson',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'August 29, 2023'
-    }]
+from .models import Post
 
 # Create your views here.
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
